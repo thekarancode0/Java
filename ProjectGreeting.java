8f5829696d6bb893bf027005105a79ad43c4412a
karan <mrkarankr87@gmail.com>
2025-07-16 08:57:25 +0530

    Adding Day1

---
+++ ProjectGreeting.java
@@ -0,0 +1,8 @@
+public class ProjectGreeting {
+    public static void main(String[] args){
+        System.out.println("Hello, I'm Karan Kumar");
+        System.out.println("I'm Passinate about Computer and Coding");
+        System.out.println("I'm learning the Java and it's day 1 of it");
+        System.out.println("I'm from Bihar, India");
+    }
+}
---
+++ first.java
@@ -0,0 +1,10 @@
+public class first {
+    public static void main(String []args){
+        String name = "karan";
+        int age = 21;
+        System.out.println("Hello World");
+        System.out.printf("Hello, I'm %s and I'm of %d year old \n",name,age);
+        System.out.print("Coding is good");
+        System.out.print("Data Structure is awesome");
+    }
+}
