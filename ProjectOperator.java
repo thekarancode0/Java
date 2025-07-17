fe240e7f88e307e269e13a3990088c3aee39f4fb
karan <mrkarankr87@gmail.com>
2025-07-17 10:37:05 +0530

    Day3

---
+++ ProjectOperator.java
@@ -0,0 +1,21 @@
+public class ProjectOperator {
+    public static void rectangle() {
+        int length = 10;
+        int width = 7;
+        System.out.println((length % 2 == 0) ? "length is even" : "length is odd");
+        System.out.println((width % 2 == 0) ? "width is even" : "width is odd");
+
+        System.out.println("Area of rectangle: " + (length * width));
+        System.out.println("Perimeter of rectangle: " + (2 * (length + width)));
+    }
+    public static void interest(){
+        int p  = 20000;
+        int r = 4;
+        int t = 3;
+        System.out.println("Simple Interest: "+ ((p*r*t)/100));
+    }
+    public static void main(String[] args) {
+        rectangle();
+        interest();
+    }
+}
