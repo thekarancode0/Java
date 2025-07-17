504aebba8679653567ee88bb48af00ea9498a15f
karan <mrkarankr87@gmail.com>
2025-07-17 10:21:44 +0530

    Day3

---
+++ operators.java
@@ -0,0 +1,42 @@
+public class operators {
+    public static void main(String[] args) {
+//        int a = 10;
+//        int b = 20;
+        //Arithmetic operator
+//        System.out.println("ADDITION: " + (a+b));
+//        System.out.println("SUBTRACTION: " + (a-b));
+//        System.out.println("MULTIPLICATION: " + (a*b));
+//        System.out.println("DIVISION: " + (b/a));
+//
+//        a++;
+//        System.out.println("Increment: " + (a));
+//
+//        short x = 99;
+//        byte y = 23;
+//        System.out.println(x == y);
+//        System.out.println(x != y);
+//        System.out.println(x >= y);
+//        System.out.println(x < y);
+
+//        int num = 5;
+//        System.out.println(~num); // -6
+//
+//        int num2 = -6;
+//        System.out.println(~num2); //5
+//
+//        int num = 20;
+//        System.out.println(num<<1); //left shift means *2
+//
+//        int numm = 7;
+//        System.out.println(numm>>1); //right shift means /2
+//        int a = 20;
+//        int b = 10;
+//        System.out.println(a^b);
+
+        int a = 100;
+        int b = 22;
+        int c = 222;
+        System.out.println(a>b && b<c);
+
+    }
+}
