e81b7c5202c55e183c728a4359f439ada45b44c4
karan <mrkarankr87@gmail.com>
2025-07-17 13:21:47 +0530

    Day4

---
+++ ifelse.java
@@ -0,0 +1,40 @@
+public class ifelse {
+    public static void main(String[] args) {
+//        int a = 10;
+//        if(a == 10){
+//            System.out.println("A is ten");
+//        }
+//        else{
+//            System.out.println("A is not ten");
+//        }
+
+//        if (true) {
+//            System.out.println("True");
+//        }
+
+//        int a = 10;
+//        int b = 20;
+//        if (a == b)
+//            if (a == 10) {
+//                System.out.println("a is 10");
+//            } else {
+//                System.out.println("a is not 10");
+//            }
+//        else {
+//            System.out.println("Outer else ");
+//        }
+
+//        int age = 18;
+//        String name = "karan";
+//        if(age >= 10){
+//            if(name.equals("karan")){
+//                System.out.println("Hey karan");
+//            }
+//            else{
+//                System.out.println("Hey Others");
+//            }
+//        }
+
+
+    }
+}
