c48e0a2d2650013951e920084756dd3633a15f2d
karan <mrkarankr87@gmail.com>
2025-07-19 07:54:41 +0530

    Day4 Project

--- Project4TrafficLight.java
+++ Project4TrafficLight.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class ProjectTrafficLight {
+public class Project4TrafficLight {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
         System.out.print("Enter the Color: ");
