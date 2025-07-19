194fb6ccf6e5f9dd5f58daadb1fdd229ae5c75d2
karan <mrkarankr87@gmail.com>
2025-07-19 07:51:38 +0530

    Day4 Project

--- ProjectTrafficLight.java
+++ ProjectTrafficLight.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class ProjectTrafficLight {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Enter the Color: ");
+        String color = read.next();
+        if(color.equals("red")){
+            System.out.println("STOP");
+        }else if(color.equals("green")){
+            System.out.println("GO");
+        }else if(color.equals("yellow")){
+            System.out.println("Slow Down");
+        }else{
+            System.out.println("Invalid color!!!!");
+        }
+    }
 }
