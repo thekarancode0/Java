f565f257760330c83465f68b58eed8c8db0b7aa1
karan <mrkarankr87@gmail.com>
2025-07-19 05:56:59 +0530

    Day4

---
+++ switchCase.java
@@ -0,0 +1,23 @@
+public class switchCase {
+    public static void main(String[] args) {
+        int option = 5;
+        switch (option){
+            case 5:
+                System.out.println("option 5");
+//                break;
+            case 4:
+                System.out.println("option 4");
+//                break;
+
+            case 3:
+                System.out.println("option 3");
+                break;
+            case 1:
+                System.out.println("Option 1");
+                break;
+
+            default:
+                System.out.println("Not matched");
+        }
+    }
+}
