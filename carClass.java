4d2c3784c6871abb03260c32be656b0761909e3f
karan <mrkarankr87@gmail.com>
2025-07-23 19:19:01 +0530

    Day 9

--- carClass.java
+++ carClass.java
@@ -10,6 +10,13 @@ public class carClass {
             this.year  = year;
         }
 
+        void start(){
+            System.out.println("Engine is starting...");
+        }
+        void drive(){
+            System.out.println("Driving...");
+        }
+
         void displayInfo(){
             System.out.println("Car Make: " + make);
             System.out.println("Car Model: " + model);
@@ -20,5 +27,7 @@ public class carClass {
     public static void main(String[] args) {
         Car c1 = new Car("Toyata","Camry",2020);
         c1.displayInfo();
+        c1.start();
+        c1.drive();
     }
 }
