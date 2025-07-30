3e848a8beda5edb9a90377bd02ab315da0d34533
karan <mrkarankr87@gmail.com>
2025-07-30 11:49:34 +0530

    Day 15 Project

---
+++ Project15Area.java
@@ -0,0 +1,41 @@
+class Shape{
+    public void CalculateArea(){
+        System.out.println("Calculating Area..");
+    }
+}
+
+class Circle extends Shape{
+    int radius;
+    Circle(int radius){
+        this.radius = radius;
+    }
+    @Override
+    public void CalculateArea(){
+        double area =  Math.PI * (radius * radius);
+        System.out.println("Area of circle of radius " + radius + " : " + area);
+    }
+}
+
+class Rectangle extends Shape{
+    private int length, width;
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+    @Override
+    public void CalculateArea(){
+        int area = length * width;
+        System.out.println("Area of Rectangle having");
+        System.out.println("Length: " + length + " and Width: " + width + " is " + area);
+    }
+}
+
+public class Project15Area {
+    public static void main(String[] args) {
+        Shape s = new Circle(5);
+        s.CalculateArea();
+
+        Shape r = new Rectangle(10,22);
+        r.CalculateArea();
+    }
+}
